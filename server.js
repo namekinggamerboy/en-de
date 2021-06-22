24e2ad674c258b5c669da958912c596adb52409f
Name boy <53903319+namekinggamerboy@users.noreply.github.com>
2021-06-22 23:48:44 +0530

    Create server.js

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
+app.get('/', function (req, res) {
+  res.sendFile(__dirname+ "/index.html");
+});
+ app.post("/en", function (req, res){
+   console.log(req.body);
+   if(req.body.uri){
+     return res.json({ success: true, output: Buffer.from(req.body.uri).toString('base64') });
+   } else {    
+  return res.json({ success: false });
+   }
+ });
+
+ app.post("/de", function (req, res){
+   if(req.body.uri){
+     return res.json({ success: true, output: Buffer.from(req.body.uri).toString('ascii')  });
+   } else {    
+  return res.json({ success: false });
+   }
+ });
+app.listen(process.env.PORT);
