0aa73a6daa21ee13feed6a8e1aa55c22b4704c05
Name boy <53903319+namekinggamerboy@users.noreply.github.com>
2021-06-22 23:51:55 +0530

    Create decode.js

---
+++ public/js/decode.js
@@ -0,0 +1,38 @@
+function de(text){
+
+}
+
+function enddesub(text){
+let h = $("#enddetext").val();
+if(h){
+fetch("/de", {
+method: "POST",
+       body: JSON.stringify({ uri: h }),
+       headers: {
+      'Content-Type': 'application/json'
+    }
+    }).then(body => body.json()).then(body => {
+
+if (body.success) {
+$("#output").text(body.output);
+} else {
+alert("😅 | try later...");
+}
+}).catch(e => {
+  console.log(e);
+  alert("Server problem");
+  });
+} else {
+alert("Enter End To End Encode!");
+}
+}
+
+ function dsub(){
+ let h = $("#detext").val();
+  if(!h){
+    $("#output").text("Enter Something..");
+  } else {   
+    let f = de(h);
+ $("#output").text(f); 
+    }
+} 
