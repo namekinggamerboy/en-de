231d46207d587c32d8436f5d001a8257b2e0b10f
Name boy <53903319+namekinggamerboy@users.noreply.github.com>
2021-06-24 16:30:52 +0530

    Update decode.js

--- public/js/decode.js
+++ public/js/decode.js
@@ -2,60 +2,60 @@ function de(tet){
  
   for (var text in tet) {
 
-if(text.includes("2")){
-
-} if(text.includes("22")){
-
+if(text.includes("2 ")){
+text = text.split("2 ").join("a "); 
+} if(text.includes("22 ")){
+text =   text.split("22 ").join("b "); 
 } if(text.includes("222")){
-
+text =   text.split("222 ").join("c "); 
 } if(text.includes("3")){
-
+text =   text.split("3 ").join("d "); 
 } if(text.includes("33")){
-
+text =   text.split("33 ").join("e "); 
 } if(text.includes("333")){
-
+text =   text.split("333 ").join("f "); 
 } if(text.includes("4")){
-
+text =   text.split("4 ").join("g "); 
 } if(text.includes("44")){
-
+text =   text.split("44 ").join("h "); 
 } if(text.includes("444")){
-
+text =   text.split("444 ").join("i "); 
 } if(text.includes("5")){
-
+text =   text.split("5 ").join("j "); 
 } if(text.includes("55")){
-
+text =   text.split("55 ").join("k "); 
 } if(text.includes("555")){
-
+text =   text.split("555 ").join("l "); 
 } if(text.includes("6")){
-
+text =   text.split("6 ").join("m "); 
 } if(text.includes("66")){
-
+text =   text.split("66 ").join("n "); 
 } if(text.includes("666")){
-
+text =   text.split("666 ").join("o "); 
 } if(text.includes("7")){
-
+text =   text.split("7 ").join("p "); 
 } if(text.includes("77")){
-
+text =   text.split("77 ").join("q "); 
 } if(text.includes("777")){
-
+text =   text.split("777 ").join("r "); 
 } if(text.includes("7777")){
-
+text =   text.split("7777 ").join("s "); 
 } if(text.includes("8")){
-
+text =   text.split("8 ").join("t "); 
 } if(text.includes("88")){
-
+text =   text.split("88 ").join("u "); 
 } if(text.includes("888")){
-
+text =   text.split("888 ").join("v "); 
 } if(text.includes("9")){
-
+text =   text.split("9 ").join("w "); 
 } if(text.includes("99")){
-
+text =   text.split("99 ").join("x "); 
 } if(text.includes("999")){
-
+text =   text.split("999 ").join("y "); 
 } if(text.includes("9999")){
-
+text =   text.split("9999 ").join("z "); 
 } if(text.includes("0")){
-
+text =   text.split("0 ").join("+ "); 
 }
 tet = text;
 }
